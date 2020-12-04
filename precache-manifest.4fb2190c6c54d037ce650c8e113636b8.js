9fb9a36cdc41fa2d1a5017d4d528a9d42575859a
Chris <chris@ChristophersMBP.fios-router.home>
2020-12-04 13:59:25 -0500

    Updates

---
+++ precache-manifest.4fb2190c6c54d037ce650c8e113636b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31df5432bb8cadee344c4fbeeb0f68a0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1fb04b8468f7725719b4",
+    "url": "/Portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "342efe4d1a9e3bd8b961",
+    "url": "/Portfolio/static/js/2.9d9a3c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.9d9a3c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb04b8468f7725719b4",
+    "url": "/Portfolio/static/js/main.3ab84840.chunk.js"
+  },
+  {
+    "revision": "095dadad0c533475cea4",
+    "url": "/Portfolio/static/js/runtime-main.f0be47bc.js"
+  }
+]);
